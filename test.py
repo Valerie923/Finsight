a8adbf2c04b5e8c80c7836035a070557ff5e72b7
Valerie <valeriechia07@gmail.com>
2026-04-01 22:35:49 +0800

    testing the apis

---
+++ test.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+from groq import Groq
+import yfinance as yf
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+    model="llama-3.3-70b-versatile",
+)
+
+print(chat_completion.choices[0].message.content)
+
+
+tickers = yf.Tickers('msft aapl goog')
+
+# access each ticker using (example)
+tickers.tickers['MSFT'].info
+tickers.tickers['AAPL'].history(period="1mo")
+tickers.tickers['GOOG'].actions
+
+# websocket
+tickers.live()
+
+print(tickers.tickers['MSFT'].info)
+
