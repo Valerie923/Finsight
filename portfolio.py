3c2f238f1368ba1a2c6ea572f003618fdaafd3bb
Valerie <valeriechia07@gmail.com>
2026-04-01 22:43:12 +0800

    done with portfolio

---
+++ portfolio.py
@@ -0,0 +1,6 @@
+portfolio = {
+    "MSFT": {"shares": 10, "avg_buy_price": 210.00},
+    "AAPL": {"shares": 5,  "avg_buy_price": 178.00},
+    "NVDA": {"shares": 3,  "avg_buy_price": 820.00},
+    "AMZN": {"shares": 8,  "avg_buy_price": 155.00},
+}
